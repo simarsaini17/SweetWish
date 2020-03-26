ffce5da72205fb3b6993c5911b1b40cb29035bdc
simarsaini17 <gursimrat2352@gmail.com>
2020-03-26 15:48:32 -0400

    Initialise project

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path=require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const webpack = require('webpack');
+
+module.exports={
+    entry:'./src/js/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+
+    devServer:{
+        contentBase:'./dist'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        })
+    ]
+
+}  
