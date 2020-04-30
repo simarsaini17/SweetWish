f39ddbecefbe6a09067e51f5dc9ff7113d9f2847
simarsaini17 <gursimrat2352@gmail.com>
2020-04-30 15:23:51 -0400

    Add selector element function

---
+++ src/js/View/selectorElements.js
@@ -0,0 +1,6 @@
+export const queryElements={
+    searchForm: document.querySelector('.head_nav'),
+    searchField : document.querySelector('.search__field'),
+    searchResult: document.querySelector('.likes__list')
+
+}
