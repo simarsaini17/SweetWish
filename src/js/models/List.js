c5915bb6763d005de3e24b814eee268cc6960e49
simarsaini17 <gursimrat2352@gmail.com>
2020-05-18 01:57:09 -0400

    Implement liked recipe list function

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,2 +1,25 @@
 import uniquid from 'uniqid';
+export default class List{
+    constructor(){
+        this.items=[];
+    }
+    addLikedItem(count,unit,ingredients){
+        const item={
+            id=uniquid(),
+            count,
+            unit,
+            ingredients
+        }
+        this.items.push(item);
+        return item;
+    }
 
+    deleteItem(id){
+        const index=this.items.findIndex(el=> el.id===id);
+        this.items.splice(index,1);
+    }
+    updateCount(id,newcount){
+        this.items.findIndex(el=> el.id===id).count=newcount;
+
+    }
+}
