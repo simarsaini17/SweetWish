54c14c7fefc18f7c94e53f8126cc8980de8d6f45
simarsaini17 <gursimrat2352@gmail.com>
2020-05-22 11:36:09 -0400

    Add add Item and delete item function

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -2,4 +2,24 @@ export default class Likes{
     constructor(){
         this.likes=[];
     }
+    addLikeItem(id,title,author,img){
+        const like={
+            id,
+            title,
+            author,
+            img
+        }
+        this.likes.push(like);
+        return like;
+    }
+    deleteItem(id){
+        const index=this.items.findIndex(el=>el.id===id);
+        this.items.splice(index,1);
+    }
+    isLiked(id){
+        return this.likes.findIndex(el=>el.id===id)!==-1;
+    }
+    getNumLikes(){
+        return this.likes.length;
+    }
 }
