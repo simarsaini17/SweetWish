4c56f730bb247623ed87d3b0e9189da7411b375d
simarsaini17 <gursimrat2352@gmail.com>
2020-05-24 17:53:49 -0400

    Render liked recipe

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -10,16 +10,28 @@ export default class Likes{
             img
         }
         this.likes.push(like);
+        this.presistData();
         return like;
     }
     deleteItem(id){
         const index=this.likes.findIndex(el=>el.id===id);
         this.likes.splice(index,1);
+        this.presistData();
+        
     }
     isLiked(id){
         return this.likes.findIndex(el=>el.id===id)!==-1;
     }
     getNumLikes(){
         return this.likes.length;
+    }
+    presistData(){
+        localStorage.setItem('likes',JSON.stringify(this.likes));
+    }
+    readData(){
+        const storage=JSON.parse(localStorage.getItem('likes'));
+        if(storage){
+            this.likes=storage;
+        }
     }
 }
